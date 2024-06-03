e077ff1b1aabc7b0394193b0d6488d380047a521
dipesh-nex <dipeshchaudhary536@gmail.com>
2024-06-03 10:14:26 +0545

    first commit

---
+++ src/main/java/com/sunaulo_bihani/SunauloBihaniApplication.java
@@ -0,0 +1,13 @@
+package com.sunaulo_bihani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunauloBihaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunauloBihaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunaulo_bihani/SunauloBihaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunaulo_bihani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunauloBihaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
